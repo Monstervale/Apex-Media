bc62b83895fc92df83b83e25bef958c86aebf0ff
MuhammedJamzeeth <mrjamzee002@gmail.com>
2025-04-17 06:00:45 +0530

    fixed: deck button rm

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,14 +35,14 @@ const MyComponent = () => {
                         </Button>
                     </Link>
 
-                    <Link href={"https://drive.google.com/file/d/1COWrP-6Aoc_ik49veeZUDkC11vgEYnsD/view"} target="_blank" rel="noopener noreferrer">
-                        <Button variant={"transparent"} className="text-[12px] md:text-[16px] font-outfit font-medium">
-                            <div className="flex items-center justify-between gap-4">
-                                {/*<Image src={'/svg/Arrow 8.svg'} alt={"arrow"} width={12.971} height={6}/>*/}
-                                <span>Deck</span>
-                            </div>
-                        </Button>
-                    </Link>
+                    {/*<Link href={"https://drive.google.com/file/d/1COWrP-6Aoc_ik49veeZUDkC11vgEYnsD/view"} target="_blank" rel="noopener noreferrer">*/}
+                    {/*    <Button variant={"transparent"} className="text-[12px] md:text-[16px] font-outfit font-medium">*/}
+                    {/*        <div className="flex items-center justify-between gap-4">*/}
+                    {/*            /!*<Image src={'/svg/Arrow 8.svg'} alt={"arrow"} width={12.971} height={6}/>*!/*/}
+                    {/*            <span>Deck</span>*/}
+                    {/*        </div>*/}
+                    {/*    </Button>*/}
+                    {/*</Link>*/}
                 </div>
 
                 <div className="font-outfit text-[#FF9A70] font-[500] text-[24px] flex">
