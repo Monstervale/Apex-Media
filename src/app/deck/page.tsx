36ba76fdd2e3c62f99cd46aee4c02c9d19d003e4
MuhammedJamzeeth <mrjamzee002@gmail.com>
2025-04-18 17:52:36 +0530

    fixed: footer at deck

--- src/app/deck/page.tsx
+++ src/app/deck/page.tsx
@@ -10,19 +10,23 @@ const ReactPdfViewer = dynamic(() => import('@/components/ReactPdfViewer'), {
 
 const DeckPage = () => {
     return (
-        <div
-            className="w-full flex flex-col items-center gap-[60px] mx-auto justify-center min-h-screen pt-[80px] lg:pt-[100px] text-white">
+        <>
+            <div
+                className="w-full flex flex-col items-center gap-[60px] mx-auto justify-center min-h-screen pt-[80px] lg:pt-[100px] text-white">
 
-            {/*<div className={"w-full flex flex-col gap-4 items-center max-w-[1440px]"}>*/}
-            {/*    {slides.map((slide) => (*/}
-            {/*        <Image key={slide.id} src={slide.src} alt={slide.alt} width={1921} height={1080}/>*/}
-            {/*    ))}*/}
-            {/*</div>*/}
+                {/*<div className={"w-full flex flex-col gap-4 items-center max-w-[1440px]"}>*/}
+                {/*    {slides.map((slide) => (*/}
+                {/*        <Image key={slide.id} src={slide.src} alt={slide.alt} width={1921} height={1080}/>*/}
+                {/*    ))}*/}
+                {/*</div>*/}
 
-            <ReactPdfViewer file="/docs/pitchdeck.pdf"/>
+                <ReactPdfViewer file="/docs/pitchdeck.pdf"/>
 
+            </div>
             <Footer/>
-        </div>
+
+        </>
+
     );
 };
 
--- src/components/ReactPdfViewer.tsx
+++ src/components/ReactPdfViewer.tsx
@@ -1,12 +1,12 @@
 // components/ReactPdfViewer.tsx
 'use client';
-import { Document, Page, pdfjs } from 'react-pdf';
+import {Document, Page, pdfjs} from 'react-pdf';
 import {useEffect, useState} from "react";
 
 // Point to PDF.js worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
-export default function ReactPdfViewer({ file }: { file: string }) {
+export default function ReactPdfViewer({file}: { file: string }) {
     const [numPages, setNumPages] = useState<number>(0);
     const [containerWidth, setContainerWidth] = useState(0);
 
@@ -17,18 +17,20 @@ export default function ReactPdfViewer({ file }: { file: string }) {
             const w = Math.min(window.innerWidth - 32, 1921);
             setContainerWidth(w);
         }
+
         window.addEventListener('resize', updateWidth);
         updateWidth();
         return () => window.removeEventListener('resize', updateWidth);
     }, []);
 
-    const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    const onDocumentLoadSuccess = ({numPages}: { numPages: number }) => {
         setNumPages(numPages);
     };
 
     return (
-        <Document className={"flex flex-col max-w-[1440px] gap-4 w-full items-center justify-center"} file={file} onLoadError={console.error} onLoadSuccess={onDocumentLoadSuccess}>
-            {Array.from({ length: numPages }, (__, index) => (
+        <Document className={"flex flex-col max-w-[1440px] gap-4 w-full items-center justify-center"} file={file}
+                  onLoadError={console.error} onLoadSuccess={onDocumentLoadSuccess}>
+            {Array.from({length: numPages}, (__, index) => (
                 <Page
                     key={`page_${index + 1}`}
                     pageNumber={index + 1}
